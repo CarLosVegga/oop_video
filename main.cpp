18dd6aa9fef6925c8495d8938b4cfe6c7bda21a2
Angel_Vázquez <a01735407@itesm.mx>
2021-05-30 21:04:55 -0500

    version1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/* INSTITUTO TECNOLÓGICO Y DE ESTUDIOS SUPERIORES DE MONTERREY
+                Campus Puebla
+Carlos Alberto Vega Pérez A01731416
+Angel Rubén Vázquez Rivera A01735407*/
+
 #include "video.cpp"
 #include "tiempo.cpp"
 #include "episodio.cpp"
